98d4491d82d5c829f9b7780a35613164ed4fa636
omitmaster <167681791+omitmaster@users.noreply.github.com>
2025-07-02 15:57:42 +0200

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/5">
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <AppSidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <AppHeader />
