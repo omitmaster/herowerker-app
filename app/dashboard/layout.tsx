539cb7e647cecc810a7411ac2ab01ad6cca01651
omitmaster <167681791+omitmaster@users.noreply.github.com>
2025-07-02 08:46:06 +0200

    Create layout.tsx

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import type { PropsWithChildren } from "react"
+
+export default function DashboardLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <AppSidebar />
+      <div className="flex flex-col">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">{children}</main>
+      </div>
+    </div>
+  )
+}
